6ca1dbcd63eeec76b66df85702d99b2bb3116d19
Shaunak Sen <shaunak1105@gmail.com>
2016-11-03 23:09:34 +0530

    created review schema

---
+++ final-project/models/reviews.js
@@ -0,0 +1,52 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+require('mongoose-currency').loadType(mongoose);
+// var Currency = mongoose.Types.Currency;
+
+
+var mcqSchema = new Schema({
+    questionNo: {
+        type: Number,
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    optionValues: [{type: Number}],
+    optionTitles: [{type: String}],
+    importance: {
+        type: Number,
+        min: 1,
+        max: 10
+    },
+    thoughtProvoking: {
+        type: Number,
+        min: 1,
+        max: 5
+    },
+    relatedTo: [{
+        // Probably use populate() here
+        questionNo: Number,
+        relatedHow: String
+    }],
+    optionValueSelected: {
+        type: Number
+    },
+    timeSpent: {
+        type: String
+    },
+    updated_at: Date
+});
+
+
+var reviewSchema = new Schema({
+    mcq: [mcqSchema],
+    postedBy: {
+        name: String,
+        email: String
+    }
+});
+
+var Reviews = mongoose.model('Review', reviewSchema);
+module.exports = Reviews;
