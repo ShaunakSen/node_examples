cf66d4cbdce3e30223400666ffa685729f48b941
Shaunak Sen <shaunak1105@gmail.com>
2016-11-09 23:48:50 +0530

    basic validation added

--- client-app/js/script.js
+++ client-app/js/script.js
@@ -4,6 +4,8 @@ myApp.controller('MainController', ['$scope', 'mainFactory', function ($scope, m
     console.log("Inside MainController");
     $scope.noOfQuestions = 0;
     $scope.questions = [];
+    $scope.response = [];
+    $scope.nextButtonDisabled = true;
     mainFactory.getQuestions().then(
         function (response) {
             console.log(response);
@@ -25,25 +27,42 @@ myApp.controller('MainController', ['$scope', 'mainFactory', function ($scope, m
 
     $scope.getIdsOfRadioButtons = function (questionNo) {
         var ids = [];
-        for(var i=0; i<$scope.noOfQuestions; ++i){
-            ids.push('option-'+questionNo+i);
+        for (var i = 0; i < $scope.noOfQuestions; ++i) {
+            ids.push('option-' + questionNo + i);
         }
         return ids
     };
 
+    $scope.radioButtonClicked = function () {
+        // change disabled state of next button now
+        $scope.nextButtonDisabled = false;
+    };
+
     $scope.storeResponse = function (questionNo) {
-        console.log(questionNo);
         // Get array of possible ids of radio buttons based on that question no
         var ids = $scope.getIdsOfRadioButtons(questionNo);
         console.log(ids);
         // check if any one of them is selected
-        for(var i=0; i<ids.length; ++i ){
-            if(document.getElementById(ids[i]).checked){
+        for (var i = 0; i < ids.length; ++i) {
+            if (document.getElementById(ids[i]).checked) {
                 var selectedRadioButton = document.getElementById(ids[i]);
-                console.log("Value: ", selectedRadioButton.value, "Text:", selectedRadioButton.getAttribute('data-text'));
+                // console.log("Value: ", selectedRadioButton.value, "Text:", selectedRadioButton.getAttribute('data-text'));
             }
         }
         // store that damn result
+        $scope.storeResult(questionNo, selectedRadioButton.value)
+
+
+        // change to next slide
+        $("#carousel-example-generic").carousel("next");
+        // disable the next button again
+        $scope.nextButtonDisabled = true;
+    };
+
+    $scope.storeResult = function (response, questionNo) {
+        console.log(response, questionNo);
+        console.log($scope.questions);
+        var responseObject = {};
     };
 
 
