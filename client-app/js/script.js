4bbe562802d75d6aa48f8a15d078906622491abb
Shaunak Sen <shaunak1105@gmail.com>
2016-11-09 17:54:07 +0530

    button functionality corrected + extracted data :)

--- client-app/js/script.js
+++ client-app/js/script.js
@@ -21,6 +21,30 @@ myApp.controller('MainController', ['$scope', 'mainFactory', function ($scope, m
         console.log("No of questions:", $scope.noOfQuestions);
         $scope.questions = data.data.mcq;
         console.log("Questions are:", $scope.questions);
-    }
+    };
+
+    $scope.getIdsOfRadioButtons = function (questionNo) {
+        var ids = [];
+        for(var i=0; i<$scope.noOfQuestions; ++i){
+            ids.push('option-'+questionNo+i);
+        }
+        return ids
+    };
+
+    $scope.storeResponse = function (questionNo) {
+        console.log(questionNo);
+        // Get array of possible ids of radio buttons based on that question no
+        var ids = $scope.getIdsOfRadioButtons(questionNo);
+        console.log(ids);
+        // check if any one of them is selected
+        for(var i=0; i<ids.length; ++i ){
+            if(document.getElementById(ids[i]).checked){
+                var selectedRadioButton = document.getElementById(ids[i]);
+                console.log("Value: ", selectedRadioButton.value, "Text:", selectedRadioButton.getAttribute('data-text'));
+            }
+        }
+        // store that damn result
+    };
+
 
 }]);
