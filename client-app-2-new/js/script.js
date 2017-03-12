d69e1f875c956131e61b6cb05bda726ecc6401cc
Shaunak Sen <shaunak1105@gmail.com>
2017-03-12 18:53:44 +0530

    create question section ideas

--- client-app-2-new/js/script.js
+++ client-app-2-new/js/script.js
@@ -8,6 +8,36 @@ myApp.config(function ($httpProvider) {
 myApp.controller('AdminController', ['$scope', '$window', '$http', function ($scope, $window, $http) {
     $scope.departments = ['IT', 'CSE', 'ECE', 'BT', 'CE', 'CHE', 'ME', 'MME'];
     
+    
+    $scope.questions = [
+        {
+            "questionNo": 1,
+            "title": "How was the coverage of the syllabus?",
+            "importance": 10,
+            "thoughtProvoking": 2,
+            "displayEmotion": true,
+            "relatedTo": [
+
+            ],
+            "optionTitles": [
+                "Very poor",
+                "Poor",
+                "Ok",
+                "Good",
+                "Very Good"
+            ],
+            "optionValues": [
+                0,
+                1,
+                2,
+                3,
+                4
+            ]
+        }
+    ];
+    
+    // On click of add question button push the element to $scope.questions
+    
 
 
 }]);
