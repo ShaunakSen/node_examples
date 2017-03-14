378219f2c7d98570d29c0def220a28304c632f4c
Shaunak Sen <shaunak1105@gmail.com>
2017-03-15 00:14:00 +0530

    cleaning up data + data type fixes

--- client-app-2-new/js/script.js
+++ client-app-2-new/js/script.js
@@ -34,11 +34,12 @@ myApp.controller('AdminController', ['$scope', '$window', '$http', function ($sc
         }
     ];
 
+
     // On click of add question button push the element to $scope.questions
-    
+
     $scope.checkSelected = function (questionNo) {
-        
-        if (document.getElementById('relationship-exists-' + questionNo).checked){
+
+        if (document.getElementById('relationship-exists-' + questionNo).checked) {
             document.getElementById('related-fields-div-' + questionNo).style.display = 'block'
         } else {
             document.getElementById('related-fields-div-' + questionNo).style.display = 'none'
@@ -51,21 +52,54 @@ myApp.controller('AdminController', ['$scope', '$window', '$http', function ($sc
 
         // Get the data
         var optionTitles = [];
+        var displayEmotion = true;
         var optionValues = [0, 1, 2, 3, 4];
         var questionNumber = questionNo;
-        var title = document.getElementById('question-title' + questionNo).value;
+        var title = document.getElementById('question-title-' + questionNo).value;
         for (var i = 0; i < 5; ++i) {
-            var optionText = document.getElementById('question-option-' + questionNo + '-' + i);
+            var optionText = document.getElementById('question-option-' + questionNo + '-' + i).value;
             optionTitles.push(optionText);
         }
-        var importance = document.getElementById('importance-' + questionNo);
-        var thoughtProvoking = document.getElementById('thought-provoking-' + questionNo);
+        var importance = parseInt(document.getElementById('importance-' + questionNo).value);
+        var thoughtProvoking = parseInt(document.getElementById('thought-provoking-' + questionNo).value);
 
+        // get related data if exists
+        if (document.getElementById('relationship-exists-' + questionNo).checked) {
+            var relatedQuestion = parseInt(document.getElementById('related-question-no-' + questionNo).value);
+            var dropdown = document.getElementById('related-dropdown-' + questionNo);
+            var relatedHow = dropdown.options[dropdown.selectedIndex].value;
+            // console.log(relatedQuestion, relatedHow);
+        }
+
+
+        // TODO: VALIDATE THE DATA
+
+        // Data is now ready
+        // Create a clean object
+
+        var questionData = {};
+
+        questionData.questionNo = questionNumber;
+        questionData.title = title;
+        questionData.importance = importance;
+        questionData.thoughtProvoking = thoughtProvoking;
+        questionData.displayEmotion = displayEmotion;
+        questionData.relatedTo = [{questionNo: null, relatedHow: null}];
+        if (document.getElementById('relationship-exists-' + questionNo).checked) {
+            questionData.relatedTo[0].questionNo = relatedQuestion;
+            questionData.relatedTo[0].relatedHow = relatedHow;
+        } else {
+            questionData.relatedTo = [];
+        }
+        questionData.optionTitles = optionTitles;
+        questionData.optionValues = optionValues;
+        console.log(questionData);
 
         if (questionNo === 1) {
             // First question submitted
-
-
+            $scope.questions[0] = questionData;
+        } else {
+            $scope.questions[questionNo - 1] = questionData;
         }
     };
 
