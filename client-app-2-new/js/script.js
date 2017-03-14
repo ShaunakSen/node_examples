6b4cceedf05683924779f3324718e82a8b5a0f79
Shaunak Sen <shaunak1105@gmail.com>
2017-03-14 13:16:25 +0530

    working with dom

--- client-app-2-new/js/script.js
+++ client-app-2-new/js/script.js
@@ -7,18 +7,16 @@ myApp.config(function ($httpProvider) {
 
 myApp.controller('AdminController', ['$scope', '$window', '$http', function ($scope, $window, $http) {
     $scope.departments = ['IT', 'CSE', 'ECE', 'BT', 'CE', 'CHE', 'ME', 'MME'];
-    
-    
+
+
     $scope.questions = [
         {
             "questionNo": 1,
-            "title": "How was the coverage of the syllabus?",
+            "title": "",
             "importance": 10,
             "thoughtProvoking": 2,
             "displayEmotion": true,
-            "relatedTo": [
-
-            ],
+            "relatedTo": [],
             "optionTitles": [
                 "Very poor",
                 "Poor",
@@ -35,9 +33,39 @@ myApp.controller('AdminController', ['$scope', '$window', '$http', function ($sc
             ]
         }
     ];
-    
+
     // On click of add question button push the element to $scope.questions
-    
+
+    $scope.relatedCheckboxClicked = function (questionNo) {
+        
+    };
+
+
+    $scope.saveQuestion = function (questionNo) {
+
+        // Get the data
+        var optionTitles = [];
+        var optionValues = [0, 1, 2, 3, 4];
+        var questionNumber = questionNo;
+        var title = document.getElementById('question-title' + questionNo).value;
+        for (var i = 0; i < 5; ++i) {
+            var optionText = document.getElementById('question-option-' + questionNo + '-' + i);
+            optionTitles.push(optionText);
+        }
+        var importance = document.getElementById('importance-' + questionNo);
+        var thoughtProvoking = document.getElementById('thought-provoking-' + questionNo);
+
+
+        if (questionNo === 1) {
+            // First question submitted
+
+
+        }
+    };
+
+    $scope.editQuestion = function (questionNo) {
+
+    }
 
 
 }]);
