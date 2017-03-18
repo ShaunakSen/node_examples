8c26fd1a20095cc1548a794637673ab8fec3872c
Shaunak Sen <shaunak1105@gmail.com>
2017-03-18 16:38:06 +0530

    admin registration

--- client-app-2-new/routes/index.js
+++ client-app-2-new/routes/index.js
@@ -13,4 +13,53 @@ router.get("/", function (req, res) {
     res.render("landing", {forms: null})
 });
 
+// TEST ROUTES
+
+router.get("/success", function (req, res) {
+    res.send("Success!!");
+});
+
+router.get("/failed", function (req, res) {
+    res.send("Failure!!");
+});
+
+
+router.post("/register", function (req, res) {
+    var newAdmin = new Admin(
+        {username: req.body.username,
+            department:req.body.department,
+            email: req.body.email,
+            full_name: req.body.full_name}
+    );
+    Admin.register(newAdmin, req.body.password, function (err, admin) {
+        // admin is newly created admin
+        if (err) {
+            console.log(err);
+            return res.render("landing");
+        }
+        passport.authenticate("local")(req, res, function () {
+            res.redirect("/");
+        });
+    });
+});
+
+// handle login logic
+
+// the middleware uses authenticate method which authenticates user..if it works it redirects somewhere else somewhere else
+
+router.post("/login", passport.authenticate("local",
+    {successRedirect: "/", failureRedirect: "/failed"}), function (req, res) {
+});
+
+// logout
+
+router.get("/logout", function (req, res) {
+    req.logout();
+    // flash message
+
+    // req.flash("success", "Logout successful");
+    res.redirect("/");
+});
+
+
 module.exports = router;
